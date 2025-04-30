4845fc2a5a0f1f5298d899d16c9164dbc67f1a9b
core <core@DESKTOP-JLDPNJO>
2025-04-30 08:50:08 -0300

    Adicionar arquivos de projeto.

---
+++ PerPlayerLoot/FakeChestDatabase.cs
@@ -0,0 +1,450 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Terraria;
+using TShockAPI;
+using TerrariaApi.Server;
+using System.IO;
+using System.IO.Streams;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Bson;
+using System.Runtime.Serialization.Formatters.Binary;
+using Microsoft.Data.Sqlite;
+using MySql.Data.MySqlClient;
+
+namespace PerPlayerLoot
+{
+    // barebones class representing item data which can be deserialized from json
+    public class JItem
+    {
+        public int id { get; set; }
+        public int stack { get; set; }
+        public byte prefix { get; set; }
+    }
+
+    // database of FakeChest's
+    public class FakeChestDatabase
+    {
+        // Map { UUID: { ChestID: Chest } }
+        public static Dictionary<string, Dictionary<int, Chest>> fakeChestsMap = new Dictionary<string, Dictionary<int, Chest>> { };
+        public static HashSet<(int, int)> playerPlacedChests = new HashSet<(int, int)>(); // tile x, y of player placed chests
+
+        private readonly string _connectionString;
+        private readonly bool _useMySQL;
+
+        public FakeChestDatabase(string connectionString, bool useMySQL = false)
+        {
+            _connectionString = connectionString;
+            _useMySQL = useMySQL;
+        }
+
+        public void Initialize()
+        {
+            CreateTables();
+            LoadFakeChests();
+        }
+
+        public void CreateTables()
+        {
+            TSPlayer.Server.SendInfoMessage("Setting up per-player chests database...");
+
+            if (_useMySQL)
+            {
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
+                {
+                    conn.Open();
+                    string sql = @"
+                        CREATE TABLE IF NOT EXISTS chests (
+                            id INTEGER NOT NULL,
+                            playerUuid TEXT NOT NULL,
+                            x INTEGER NOT NULL,
+                            y INTEGER NOT NULL,
+                            items LONGBLOB NOT NULL,
+                            PRIMARY KEY (id, playerUuid)
+                        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+                        CREATE TABLE IF NOT EXISTS placed (
+                            x INTEGER NOT NULL,
+                            y INTEGER NOT NULL,
+                            PRIMARY KEY (x, y)
+                        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+                    ";
+                    using (var cmd = new MySqlCommand(sql, conn))
+                        cmd.ExecuteNonQuery();
+                }
+            }
+            else
+            {
+                using (SqliteConnection conn = new SqliteConnection(_connectionString))
+                {
+                    conn.Open();
+                    string sql = @"
+                        CREATE TABLE IF NOT EXISTS chests (
+                            id INTEGER NOT NULL,
+                            playerUuid TEXT NOT NULL,
+                            x INTEGER NOT NULL,
+                            y INTEGER NOT NULL,
+                            items BLOB NOT NULL,
+                            PRIMARY KEY (id, playerUuid)
+                        );
+
+                        CREATE TABLE IF NOT EXISTS placed (
+                            x INTEGER NOT NULL,
+                            y INTEGER NOT NULL,
+                            PRIMARY KEY (x, y)
+                        );
+                    ";
+                    using (var cmd = new SqliteCommand(sql, conn))
+                        cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void LoadFakeChests()
+        {
+            TSPlayer.Server.SendInfoMessage("Loading per-player loot chest inventories...");
+            int count = 0;
+
+            if (_useMySQL)
+            {
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
+                {
+                    conn.Open();
+
+                    // load loot chests
+                    using (var cmd = new MySqlCommand("SELECT id, playerUuid, x, y, items FROM chests;", conn))
+                    {
+                        MySqlDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            string playerUuid = Convert.ToString(reader["playerUuid"]);
+                            int chestId = Convert.ToInt32(reader["id"]);
+
+                            // get the items list
+                            List<Item> items = new List<Item>();
+
+                            // read blob from column
+                            MemoryStream itemsRaw = new MemoryStream((byte[])reader["items"]);
+                            // deserialize with bson
+                            using (var br = new BsonReader(itemsRaw))
+                            {
+                                br.ReadRootValueAsArray = true;
+
+                                // do the actual deserialization
+                                var jItems = (new JsonSerializer()).Deserialize<IList<JItem>>(br);
+
+                                // convert each JItem to a real Item
+                                foreach (var jItem in jItems)
+                                {
+                                    if (jItem == null)
+                                    {
+                                        items.Add(null);
+                                        continue;
+                                    }
+
+                                    var item = new Item();
+                                    item.netDefaults(jItem.id);
+                                    item.stack = jItem.stack;
+                                    item.prefix = jItem.prefix;
+
+                                    items.Add(item);
+                                }
+                            }
+
+                            Chest chest = new Chest(); // construct a terraria chest
+                            chest.x = Convert.ToInt32(reader["x"]);
+                            chest.y = Convert.ToInt32(reader["y"]);
+                            chest.item = items.ToArray();
+
+                            // save it in the fake chest map
+                            var playerChests = fakeChestsMap.GetValueOrDefault(playerUuid, new Dictionary<int, Chest>());
+                            fakeChestsMap[playerUuid] = playerChests;
+
+                            fakeChestsMap[playerUuid][chestId] = chest;
+
+                            count++;
+                        }
+                    }
+
+                    // load tile exclusions
+                    using (var cmd = new MySqlCommand("SELECT x, y FROM placed;", conn))
+                    {
+                        MySqlDataReader reader = cmd.ExecuteReader();
+
+                        playerPlacedChests.Clear();
+
+                        while (reader.Read())
+                        {
+                            int x = Convert.ToInt32(reader["x"]);
+                            int y = Convert.ToInt32(reader["y"]);
+
+                            playerPlacedChests.Add((x, y));
+                        }
+                    }
+                }
+            }
+            else
+            {
+                using (SqliteConnection conn = new SqliteConnection(_connectionString))
+                {
+                    conn.Open();
+
+                    // load loot chests
+                    using (var cmd = new SqliteCommand("SELECT id, playerUuid, x, y, items FROM chests;", conn))
+                    {
+                        SqliteDataReader reader = cmd.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            string playerUuid = Convert.ToString(reader["playerUuid"]);
+                            int chestId = Convert.ToInt32(reader["id"]);
+
+                            // get the items list
+                            List<Item> items = new List<Item>();
+
+                            // read blob from column
+                            MemoryStream itemsRaw = new MemoryStream((byte[])reader["items"]);
+                            // deserialize with bson
+                            using (var br = new BsonReader(itemsRaw))
+                            {
+                                br.ReadRootValueAsArray = true;
+
+                                // do the actual deserialization
+                                var jItems = (new JsonSerializer()).Deserialize<IList<JItem>>(br);
+
+                                // convert each JItem to a real Item
+                                foreach (var jItem in jItems)
+                                {
+                                    if (jItem == null)
+                                    {
+                                        items.Add(null);
+                                        continue;
+                                    }
+
+                                    var item = new Item();
+                                    item.netDefaults(jItem.id);
+                                    item.stack = jItem.stack;
+                                    item.prefix = jItem.prefix;
+
+                                    items.Add(item);
+                                }
+                            }
+
+                            Chest chest = new Chest(); // construct a terraria chest
+                            chest.x = Convert.ToInt32(reader["x"]);
+                            chest.y = Convert.ToInt32(reader["y"]);
+                            chest.item = items.ToArray();
+
+                            // save it in the fake chest map
+                            var playerChests = fakeChestsMap.GetValueOrDefault(playerUuid, new Dictionary<int, Chest>());
+                            fakeChestsMap[playerUuid] = playerChests;
+
+                            fakeChestsMap[playerUuid][chestId] = chest;
+
+                            count++;
+                        }
+                    }
+
+                    // load tile exclusions
+                    using (var cmd = new SqliteCommand("SELECT x, y FROM placed;", conn))
+                    {
+                        SqliteDataReader reader = cmd.ExecuteReader();
+
+                        playerPlacedChests.Clear();
+
+                        while (reader.Read())
+                        {
+                            int x = Convert.ToInt32(reader["x"]);
+                            int y = Convert.ToInt32(reader["y"]);
+
+                            playerPlacedChests.Add((x, y));
+                        }
+                    }
+                }
+            }
+
+            TSPlayer.Server.SendSuccessMessage($"Loaded {count} loot chest inventories, {playerPlacedChests.Count} player-placed chests.");
+        }
+
+        public void SaveFakeChests()
+        {
+            TSPlayer.Server.SendInfoMessage("Saving per-player loot chest inventories...");
+            int count = 0;
+
+            if (_useMySQL)
+            {
+                using (MySqlConnection conn = new MySqlConnection(_connectionString))
+                {
+                    conn.Open();
+
+                    foreach (KeyValuePair<string, Dictionary<int, Chest>> playerEntry in fakeChestsMap)
+                    {
+                        string playerUuid = playerEntry.Key;
+                        var playerChests = playerEntry.Value;
+
+                        foreach (KeyValuePair<int, Chest> chestEntry in playerChests)
+                        {
+                            int chestId = chestEntry.Key;
+                            var chest = chestEntry.Value;
+
+                            List<JItem> jItems = new List<JItem>(chest.item.Length);
+
+                            foreach (var item in chest.item)
+                            {
+                                var jItem = new JItem();
+
+                                jItem.id = item.type;
+                                jItem.stack = item.stack;
+                                jItem.prefix = item.prefix;
+
+                                jItems.Add(jItem);
+                            }
+
+                            MemoryStream itemsMs = new MemoryStream();
+                            using (var writer = new BsonWriter(itemsMs))
+                            {
+                                JsonSerializer serializer = new JsonSerializer();
+                                serializer.Serialize(writer, jItems);
+                            }
+
+                            var sql = @"REPLACE INTO chests (id, playerUuid, x, y, items) VALUES (@id, @playerUuid, @x, @y, @items);";
+
+                            using (var cmd = new MySqlCommand(sql, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@id", chestId);
+                                cmd.Parameters.AddWithValue("@playerUuid", playerUuid);
+                                cmd.Parameters.AddWithValue("@x", chest.x);
+                                cmd.Parameters.AddWithValue("@y", chest.y);
+                                cmd.Parameters.AddWithValue("@items", itemsMs.ToArray());
+
+                                cmd.ExecuteNonQuery();
+                            }
+
+                            count++;
+                        }
+                    }
+
+                    foreach ((int x, int y) in playerPlacedChests)
+                    {
+                        var sql = @"REPLACE INTO placed (x, y) VALUES (@x, @y);";
+
+                        using (var cmd = new MySqlCommand(sql, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@x", x);
+                            cmd.Parameters.AddWithValue("@y", y);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                using (SqliteConnection conn = new SqliteConnection(_connectionString))
+                {
+                    conn.Open();
+
+                    foreach (KeyValuePair<string, Dictionary<int, Chest>> playerEntry in fakeChestsMap)
+                    {
+                        string playerUuid = playerEntry.Key;
+                        var playerChests = playerEntry.Value;
+
+                        foreach (KeyValuePair<int, Chest> chestEntry in playerChests)
+                        {
+                            int chestId = chestEntry.Key;
+                            var chest = chestEntry.Value;
+
+                            List<JItem> jItems = new List<JItem>(chest.item.Length);
+
+                            foreach (var item in chest.item)
+                            {
+                                var jItem = new JItem();
+
+                                jItem.id = item.type;
+                                jItem.stack = item.stack;
+                                jItem.prefix = item.prefix;
+
+                                jItems.Add(jItem);
+                            }
+
+                            MemoryStream itemsMs = new MemoryStream();
+                            using (var writer = new BsonWriter(itemsMs))
+                            {
+                                JsonSerializer serializer = new JsonSerializer();
+                                serializer.Serialize(writer, jItems);
+                            }
+
+                            var sql = @"REPLACE INTO chests (id, playerUuid, x, y, items) VALUES (@id, @playerUuid, @x, @y, @items);";
+
+                            using (var cmd = new SqliteCommand(sql, conn))
+                            {
+                                cmd.Parameters.AddWithValue("@id", chestId);
+                                cmd.Parameters.AddWithValue("@playerUuid", playerUuid);
+                                cmd.Parameters.AddWithValue("@x", chest.x);
+                                cmd.Parameters.AddWithValue("@y", chest.y);
+                                cmd.Parameters.AddWithValue("@items", itemsMs.ToArray());
+
+                                cmd.ExecuteNonQuery();
+                            }
+
+                            count++;
+                        }
+                    }
+
+                    foreach ((int x, int y) in playerPlacedChests)
+                    {
+                        var sql = @"REPLACE INTO placed (x, y) VALUES (@x, @y);";
+
+                        using (var cmd = new SqliteCommand(sql, conn))
+                        {
+                            cmd.Parameters.AddWithValue("@x", x);
+                            cmd.Parameters.AddWithValue("@y", y);
+
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+
+            TSPlayer.Server.SendSuccessMessage($"Saved {count} loot chest inventories, {playerPlacedChests.Count} player-placed chests.");
+        }
+
+        public Chest GetOrCreateFakeChest(int chestId, string playerUuid)
+        {
+            var playerChests = fakeChestsMap.GetValueOrDefault(playerUuid, new Dictionary<int, Chest>());
+            fakeChestsMap[playerUuid] = playerChests;
+
+            if (!playerChests.ContainsKey(chestId))
+            {
+                Chest realChest = Main.chest[chestId];
+
+                // copy the chest data from the real untouched chest
+                Chest fakeChest = new Chest();
+                fakeChest.x = realChest.x;
+                fakeChest.y = realChest.y;
+                realChest.item.CopyTo(fakeChest.item, 0);
+
+                // save it in the fake chest list
+                fakeChestsMap[playerUuid][chestId] = fakeChest;
+
+                // save the fake chests list to disk
+                SaveFakeChests();
+
+                return fakeChest;
+            }
+
+            return playerChests[chestId];
+        }
+
+        public void SetChestPlayerPlaced(int tileX, int tileY)
+        {
+            playerPlacedChests.Add((tileX, tileY));
+        }
+
+        public bool IsChestPlayerPlaced(int tileX, int tileY)
+        {
+            return playerPlacedChests.Contains((tileX, tileY));
+        }
+    }
+}
---
+++ PerPlayerLoot/PPLPlugin.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using Terraria;
+using TShockAPI;
+using TerrariaApi.Server;
+
+namespace PerPlayerLoot
+{
+    [ApiVersion(2, 1)]
+    public class PPLPlugin : TerrariaPlugin
+    {
+        #region info
+        public override string Name => "PerPlayerLoot";
+
+        public override Version Version => new Version(2, 0);
+
+        public override string Author => "Codian atualized by brasilzinhoz";
+
+        public override string Description => "Duplicate loot chest inventories for each player.";
+        #endregion
+
+        public static FakeChestDatabase fakeChestDb;
+        public static bool enablePpl = true;
+
+        private const string ConfigPath = "tshock/perplayerloot_config.json";
+        private PluginConfig config;
+
+        public PPLPlugin(Main game) : base(game) { }
+
+        public override void Initialize()
+        {
+            LoadConfig();
+            InitializeDatabase();
+
+            ServerApi.Hooks.GamePostInitialize.Register(this, OnWorldLoaded);
+            ServerApi.Hooks.WorldSave.Register(this, OnWorldSave);
+
+            TShockAPI.GetDataHandlers.PlaceChest += OnChestPlace;
+            TShockAPI.GetDataHandlers.ChestOpen += OnChestOpen;
+            TShockAPI.GetDataHandlers.ChestItemChange += OnChestItemChange;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GamePostInitialize.Deregister(this, OnWorldLoaded);
+                ServerApi.Hooks.WorldSave.Deregister(this, OnWorldSave);
+
+                TShockAPI.GetDataHandlers.PlaceChest -= OnChestPlace;
+                TShockAPI.GetDataHandlers.ChestOpen -= OnChestOpen;
+                TShockAPI.GetDataHandlers.ChestItemChange -= OnChestItemChange;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void LoadConfig()
+        {
+            if (!File.Exists(ConfigPath))
+            {
+                config = new PluginConfig();
+                File.WriteAllText(ConfigPath, JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true }));
+            }
+            else
+            {
+                var json = File.ReadAllText(ConfigPath);
+                config = JsonSerializer.Deserialize<PluginConfig>(json) ?? new PluginConfig();
+            }
+        }
+
+        private void InitializeDatabase()
+        {
+            if (config.DatabaseType.Equals("MySQL", StringComparison.OrdinalIgnoreCase))
+            {
+                string connectionString = $"Server={config.MySQL.Host};Port={config.MySQL.Port};Database={config.MySQL.Database};Uid={config.MySQL.Username};Pwd={config.MySQL.Password};";
+                fakeChestDb = new FakeChestDatabase(connectionString, true);
+                TSPlayer.Server.SendInfoMessage("Using MySQL database for per-player loot.");
+            }
+            else
+            {
+                fakeChestDb = new FakeChestDatabase("Data Source=tshock/perplayerloot.sqlite");
+                TSPlayer.Server.SendInfoMessage("Using SQLite database for per-player loot.");
+            }
+        }
+
+        private void OnWorldSave(WorldSaveEventArgs args)
+        {
+            fakeChestDb.SaveFakeChests();
+        }
+
+        private void OnWorldLoaded(EventArgs args)
+        {
+            fakeChestDb.Initialize();
+            Commands.ChatCommands.Add(new Command("perplayerloot.toggle", ToggleCommand, "ppltoggle"));
+        }
+
+        private void ToggleCommand(CommandArgs args)
+        {
+            enablePpl = !enablePpl;
+            if (enablePpl)
+            {
+                args.Player.SendSuccessMessage("Per player loot is now enabled!");
+            }
+            else
+            {
+                args.Player.SendSuccessMessage("Per player loot is now disabled! You can modify chests now and they will count as loot chests.");
+            }
+        }
+
+        private void OnChestItemChange(object sender, GetDataHandlers.ChestItemEventArgs e)
+        {
+            if (!enablePpl) return;
+
+            Chest realChest = Main.chest[e.ID];
+            if (realChest == null) return;
+
+            if (realChest.bankChest) return;
+
+            if (fakeChestDb.IsChestPlayerPlaced(realChest.x, realChest.y)) return;
+
+            Item item = new Item();
+            item.netDefaults(e.Type);
+            item.stack = e.Stacks;
+            item.prefix = e.Prefix;
+
+            Chest fakeChest = fakeChestDb.GetOrCreateFakeChest(e.ID, e.Player.UUID);
+            fakeChest.item[e.Slot] = item;
+
+            e.Handled = true;
+        }
+
+        private byte[] ConstructSpoofedChestItemPacket(int chestId, int slot, Item item)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            OTAPI.PacketWriter packetWriter = new OTAPI.PacketWriter(memoryStream);
+
+            packetWriter.BaseStream.Position = 0L;
+            long position = packetWriter.BaseStream.Position;
+
+            packetWriter.BaseStream.Position += 2L;
+            packetWriter.Write((byte)PacketTypes.ChestItem);
+
+            packetWriter.Write((short)chestId);
+            packetWriter.Write((byte)slot);
+
+            short netId = (short)item.netID;
+            if (item.Name == null)
+            {
+                netId = 0;
+            }
+
+            packetWriter.Write((short)item.stack);
+            packetWriter.Write(item.prefix);
+            packetWriter.Write(netId);
+
+            int positionAfter = (int)packetWriter.BaseStream.Position;
+
+            packetWriter.BaseStream.Position = position;
+            packetWriter.Write((ushort)positionAfter);
+            packetWriter.BaseStream.Position = positionAfter;
+
+            return memoryStream.ToArray();
+        }
+
+        private void OnChestOpen(object sender, GetDataHandlers.ChestOpenEventArgs e)
+        {
+            if (e.Handled) return;
+            if (!enablePpl) return;
+
+            int chestId = Chest.FindChest(e.X, e.Y);
+            if (chestId == -1) return;
+
+            Chest realChest = Main.chest[chestId];
+            if (realChest == null) return;
+
+            if (realChest.bankChest) return;
+
+            if (fakeChestDb.IsChestPlayerPlaced(realChest.x, realChest.y)) return;
+
+            Chest fakeChest = fakeChestDb.GetOrCreateFakeChest(chestId, e.Player.UUID);
+
+            e.Player.SendInfoMessage("[c/FF0000:O loot neste baú é salvo por jogador!]");
+
+            for (int slot = 0; slot < Chest.maxItems; slot++)
+            {
+                Item item = fakeChest.item[slot];
+                byte[] payload = ConstructSpoofedChestItemPacket(chestId, slot, item);
+                e.Player.SendRawData(payload);
+            }
+
+            e.Player.SendData(PacketTypes.ChestOpen, "", chestId);
+            e.Player.ActiveChest = chestId;
+            Main.player[e.Player.Index].chest = chestId;
+            e.Player.SendData(PacketTypes.SyncPlayerChestIndex, null, e.Player.Index, chestId);
+
+            e.Handled = true;
+        }
+
+        private void OnChestPlace(object sender, GetDataHandlers.PlaceChestEventArgs e)
+        {
+            if (!enablePpl) return;
+
+            if (!fakeChestDb.IsChestPlayerPlaced(e.TileX, e.TileY - 1))
+            {
+                int chestId = Chest.FindChest(e.TileX, e.TileY - 1);
+                if (chestId != -1)
+                    Main.chest[chestId].item = new Item[Chest.maxItems];
+            }
+
+            fakeChestDb.SetChestPlayerPlaced(e.TileX, e.TileY - 1);
+        }
+    }
+
+    public class PluginConfig
+    {
+        public string DatabaseType { get; set; } = "SQLite";
+        public MySQLConfig MySQL { get; set; } = new MySQLConfig();
+    }
+
+    public class MySQLConfig
+    {
+        public string Host { get; set; } = "localhost";
+        public int Port { get; set; } = 3306;
+        public string Database { get; set; } = "perplayerloot";
+        public string Username { get; set; } = "root";
+        public string Password { get; set; } = "password";
+    }
+}
